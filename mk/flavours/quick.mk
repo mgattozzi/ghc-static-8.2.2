88d9e3d0c0b69f07fd13d12a3149ce4255c20759
Michael Gattozzi <mgattozzi@gmail.com>
2018-01-24 10:21:21 -0500

    Initial commit
    Don't send issues here. This is only here for use in ghc-rts-rs.

---
+++ mk/flavours/quick.mk
@@ -0,0 +1,11 @@
+SRC_HC_OPTS        = -O0 -H64m -fPIC
+GhcStage1HcOpts    = -O -fPIC
+GhcStage2HcOpts    = -O0 -fPIC
+GhcLibHcOpts       = -O -fPIC
+BUILD_PROF_LIBS    = NO
+SplitObjs          = NO
+SplitSections      = NO
+HADDOCK_DOCS       = NO
+BUILD_SPHINX_HTML  = NO
+BUILD_SPHINX_PDF   = NO
+BUILD_MAN          = NO
