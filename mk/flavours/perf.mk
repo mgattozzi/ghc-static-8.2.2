f69b670f25a68b6182a7d8eebfe3c58883878250
Michael Gattozzi <mgattozzi@gmail.com>
2018-01-24 10:21:21 -0500

    Initial commit
    Don't send issues here. This is only here for use in ghc-rts-rs.

---
+++ mk/flavours/perf.mk
@@ -0,0 +1,9 @@
+SRC_HC_OPTS        = -O -H64m -fPIC
+GhcStage1HcOpts    = -O -fPIC
+GhcStage2HcOpts    = -O2 -fPIC
+GhcLibHcOpts       = -O2 -fPIC
+BUILD_PROF_LIBS    = YES
+#SplitObjs
+#HADDOCK_DOCS
+#BUILD_SPHINX_HTML
+#BUILD_SPHINX_PDF
